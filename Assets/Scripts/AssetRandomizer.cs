1a2413cb41dbdf5fa61ec503490b3323840573d4
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-05-10 22:54:06 -0700

    Added new generation, removed world editor-preview

---
+++ Assets/Scripts/AssetRandomizer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AssetRandomizer : MonoBehaviour
+{
+    public List<GameObject> SourceObjects;
+
+    public List<Sprite> TopSprites;
+    public List<Sprite> MidSprites;
+    public List<Sprite> BotSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < SourceObjects.Count; i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    SourceObjects[i].GetComponent<SpriteRenderer>().sprite = TopSprites[Random.Range(0, TopSprites.Count)];
+                    break;
+                case 1:
+                    SourceObjects[i].GetComponent<SpriteRenderer>().sprite = MidSprites[Random.Range(0, MidSprites.Count)];
+                    break;
+                case 2:
+                    SourceObjects[i].GetComponent<SpriteRenderer>().sprite = BotSprites[Random.Range(0, BotSprites.Count)];
+                    break;
+            }
+        }
+    }
+}
