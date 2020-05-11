c70e41e3194f68ec86dc53e20b2a783e32a792f5
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-05-11 15:45:37 -0700

    Added 2x2 assets

--- Assets/Scripts/AssetRandomizer.cs
+++ Assets/Scripts/AssetRandomizer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+// [ExecuteInEditMode]
 public class AssetRandomizer : MonoBehaviour
 {
     public List<GameObject> SourceObjects;
