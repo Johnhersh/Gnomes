c0d61f12186ea11b5041c20715f9a1baea0d18ac
John Hershberg <johnher@gmail.com>
2021-04-24 20:11:42 -0700

    Fixed flickering issue

--- Assets/Scripts/PositionRendererSorterStatic.cs
+++ Assets/Scripts/PositionRendererSorterStatic.cs
@@ -10,13 +10,27 @@ public class PositionRendererSorterStatic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        SetSortingOrder();
+        SetZPosition();
+    }
+
+    /// <summary>
+    /// Set this sprite's sorting order based on its Y position in the world
+    /// </summary>
+    private void SetSortingOrder()
+    {
         _myRenderer = gameObject.GetComponent<Renderer>();
         _myRenderer.sortingOrder = (int)(_sortingOrderBase - transform.position.y + _offset);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Objects with the same sorting order will be sorted based on their Z position
+    /// This will move them in 3D space so adjacent objects won't flicker as they're being sorted
+    /// </summary>
+    private void SetZPosition()
     {
-
+        var myPosition = transform.parent.position;
+        myPosition.z = myPosition.x / 10;
+        transform.parent.position = myPosition;
     }
 }
